b90da10193fa351e8be5bc43f63f067cefd26133
Thiago Luiz <thiagoluizst14@gmail.com>
2025-05-23 16:41:46 -0300

    Plotly Funcional

--- FyDataScience/FyDS/views.py
+++ FyDataScience/FyDS/views.py
@@ -4,14 +4,76 @@ from django.views.generic import *
 from deep_translator import GoogleTranslator
 import numpy as np
 import pandas as pd
+from plotly import plot
 import yfinance as yf
+import plotly.io as pio
 import plotly.graph_objs as go
 
 
 # Criando Modelos CBV(Class-Based-View)
+class HomePage(View):
+    def get(self, request, *args, **kwargs):
+        tech = yf.Sector('healthcare')
+        df = tech.top_companies.head(6).reset_index()
+        df.rename(columns={'index': 'symbol'}, inplace=True)
 
-class HomePage(TemplateView):
-    template_name = 'base.html'
+        dados = df.to_dict(orient='records')
+
+        for empresa in dados:
+            symbol = empresa['symbol']
+            ticker = yf.Ticker(symbol)
+            hist = ticker.history(period='1mo')
+
+            if hist.empty:
+                empresa['grafico_html'] = "<p>Sem dados</p>"
+                continue
+
+            ymin = min(hist['Open'].min(), hist['Close'].min())
+            ymax = max(hist['Open'].max(), hist['Close'].max())
+
+            fig = go.Figure()
+
+            # Linha de Abertura
+            fig.add_trace(go.Scatter(
+                x=hist.index,
+                y=hist['Open'],
+                mode='lines+markers',
+                name='Abertura',
+                line=dict(color='orange'),
+                hovertemplate='Data: %{x}<br>Abertura: R$ %{y:.2f}<extra></extra>'
+            ))
+
+            # Linha de Fechamento
+            fig.add_trace(go.Scatter(
+                x=hist.index,
+                y=hist['Close'],
+                mode='lines+markers',
+                name='Fechamento',
+                line=dict(color='royalblue'),
+                hovertemplate='Data: %{x}<br>Fechamento: R$ %{y:.2f}<extra></extra>'
+            ))
+
+            fig.update_layout(
+                height=300,
+                width=525,
+                margin=dict(l=20, r=20, t=30, b=30),
+                xaxis_title='Data',
+                yaxis_title='Preço',
+                template='plotly_white',
+                showlegend=True,
+                yaxis=dict(
+                    range=[ymin * 0.98, ymax * 1.02],  # margem de 2%
+                    tickprefix="R$ ",
+                    tickformat=".2f"
+                )
+            )
+
+            empresa['grafico_html'] = pio.to_html(fig, full_html=False, include_plotlyjs=False)
+
+        # Organiza em uma matriz 3x2 (6 elementos -> 3 linhas de 2)
+        dados_setores = [dados[i:i + 2] for i in range(0, len(dados), 2)]
+
+        return render(request, 'home.html', {'dados_setores': dados_setores})
 
 
 class StockView(View):
@@ -24,6 +86,7 @@ class StockView(View):
             'erro': None,
             'info_geral': {},
         })
+
 
     def post(self, request):
         erro = None
@@ -40,7 +103,7 @@ class StockView(View):
         simbolo = request.POST.get('simbolo')
         periodo = request.POST.get('periodo', '1d')
         translated_summary = 'Não disponível'
-
+    
         try:
             acao = yf.Ticker(simbolo)
             dados = acao.history(period=periodo)
