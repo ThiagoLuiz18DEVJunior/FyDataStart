d92319911e1eeacf8250a41fdefe796320dba158
ThiagoLuiz18DEVJunior <thiagoluizst14@gmail.com>
2025-07-20 16:43:09 -0300

    fix static files

--- FyDataScience/FyDataScience/settings.py
+++ FyDataScience/FyDataScience/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-93tz*5k%@jmpux417ulj2yal1dqafv77)$r+&7wet%08ib3d=+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.fydatastart.com.br', 'fydatastart-production.up.railway.app', 'localhost', '127.0.0.1']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,9 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # onde os arquivos irão após collectstatic
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# WhiteNoise para servir estáticos em produção
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
