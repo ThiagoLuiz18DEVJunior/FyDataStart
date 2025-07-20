5928dad329050e304511ab216de7a885bba90fc1
ThiagoLuiz18DEVJunior <thiagoluizst14@gmail.com>
2025-07-20 16:01:31 -0300

    Alterando settings

--- FyDataScience/FyDataScience/settings.py
+++ FyDataScience/FyDataScience/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-93tz*5k%@jmpux417ulj2yal1dqafv77)$r+&7wet%08ib3d=+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.fydatastart.com.br', 'fydatastart-production.up.railway.app', 'localhost', '127.0.0.1']
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
